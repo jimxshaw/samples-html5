c61e60df1ad426bf1ea0c5cfc829a58c77df6355
Jim Shaw <jimxshaw@gmail.com>
2015-04-04 19:38:46 -0500

    finished html5 drap & drop sample

---
+++ drag-drop/js/script.js
@@ -0,0 +1,16 @@
+if (window.FileReader && Modernizr.draganddrop) {
+    function allowDrop(ev) {
+      ev.preventDefault();
+    }
+    function drag(ev) {
+      ev.dataTransfer.setData("Text", ev.target.id);
+    }
+    function drop(ev) {
+      ev.preventDefault();
+      var data = ev.dataTransfer.getData("Text");
+      ev.target.appendChild(document.getElementById(data));
+    }
+  }
+  else {
+    document.write('This browser cannot use drag and drop');
+}
