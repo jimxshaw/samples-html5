d9849f2b99ff68ad73f6fbf6936f53d10d1d756b
Jim Shaw <jimxshaw@gmail.com>
2015-04-04 20:15:47 -0500

    added html5 video sample

--- dev-directory/complete-site/js/script.js
+++ dev-directory/complete-site/js/script.js
@@ -1,3 +1,52 @@
 function updateTextInput(val) {
   document.getElementById('textInput').value = val;
 }
+
+var myVideo = document.getElementById('ad');
+
+function playPause() {
+  if (myVideo.paused) {
+    myVideo.play();  
+  }
+  else {
+    myVideo.pause();
+  }
+}
+
+function makeBig() {
+  myVideo.width = 640;
+}
+
+function makeSmall() {
+  myVideo.width = 360;
+}
+
+function makeNormal() {
+  myVideo.width = 480;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
