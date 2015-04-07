eb7d40d1288e43791e4c53c776222fe1b8d3f592
Jim Shaw <jimxshaw@gmail.com>
2015-04-06 19:07:30 -0500

    finished adding tasks

---
+++ dev-directory/daily-planner/js/script.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+  $('#add-task-form').on('submit', function(e) {
+    addTask(e);
+  });
+
+  // Function to add a task
+  function addTask(e) {
+    // Add unique ID
+    var newDate = new Date();
+    id = newDate.getTime();
+
+    var task = $('#task').val();
+    var task_priority = $('#priority').val();
+    var task_date = $('#date').val();
+    var task_time = $('#time').val();
+
+    // Simple validation
+    if (task == "") {
+      alert("Task is required");
+      e.preventDefault();
+    }
+    else if (task_date == "") {
+      alert("Date is required");
+      e.preventDefault();
+    }
+    else if (task_time == "") {
+      alert("Time is required");
+      e.preventDefault();
+    }
+    else if (task_priority == "") {
+      task_priority = 'normal';
+    }
+    else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+
+      // Check tasks
+      if (tasks == null) {
+        tasks = [];
+      }
+      var taskList = JSON.parse(localStorage.getItem('tasks'));
+
+      // New task object
+      var new_task = {
+        "id": id,
+        "task": task,
+        "task_priority": task_priority,
+        "task_date": task_date,
+        "task_time": task_time
+      };
+
+      tasks.push(new_task);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+
+      console.log("task added!"); 
+    }
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
