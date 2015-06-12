f1dcf0163afa84093cedb4183f49a4edc74b3110
Jim Shaw <jimxshaw@gmail.com>
2015-06-12 17:58:05 -0500

    finished jquery

--- quiz-app/js/script.js
+++ quiz-app/js/script.js
@@ -8,7 +8,7 @@ var point = 1;
 var highest = total * point;
 
 // Initializer
-function() {
+function init() {
   // Set correct answers
   sessionStorage.setItem('a1', 'c');
   sessionStorage.setItem('a2', 'd');
@@ -26,34 +26,80 @@ $(document).ready(function() {
 
   $('#q1 #submit').click(function() {
     $('.questionForm').hide();
+    process('q1');
     $('#q2').fadeIn(300);
     return false;
   });
 
   $('#q2 #submit').click(function() {
     $('.questionForm').hide();
+    process('q2');
     $('#q3').fadeIn(300);
     return false;
   });
 
   $('#q3 #submit').click(function() {
     $('.questionForm').hide();
+    process('q3');
     $('#q4').fadeIn(300);
     return false;
   });
 
   $('#q4 #submit').click(function() {
     $('.questionForm').hide();
+    process('q4');
     $('#q5').fadeIn(300);
     return false;
   });
 
   $('#q5 #submit').click(function() {
     $('.questionForm').hide();
+    process('q5');
     $('#results').fadeIn(300);
     return false;
   });
 });
+
+// Process the answers
+function process(q) {
+  if (q == "q1") {
+    var submitted  = $('input[name = q1]:checked').val();
+    if (submitted == sessionStorage.a1) {
+      score++;
+    }
+  }
+
+  if (q == "q2") {
+    var submitted  = $('input[name = q2]:checked').val();
+    if (submitted == sessionStorage.a2) {
+      score++;
+    }
+  }
+
+  if (q == "q3") {
+    var submitted  = $('input[name = q3]:checked').val();
+    if (submitted == sessionStorage.a3) {
+      score++;
+    }
+  }
+
+  if (q == "q4") {
+    var submitted  = $('input[name = q4]:checked').val();
+    if (submitted == sessionStorage.a4) {
+      score++;
+    }
+  }
+
+  if (q == "q5") {
+    var submitted  = $('input[name = q5]:checked').val();
+    if (submitted == sessionStorage.a5) {
+      score++;
+    }
+
+    $('#results').html('<h3>Your final score is: ' + score + ' out of 5</h3><a href="index.html">Take Quiz Again</a>');
+  }
+  return false;
+}
 
 // Add event listener
 window.addEventListener('load', init, false);
