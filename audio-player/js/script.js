f075b70a00b115a901f63a8cdc5a3abb7c8361e9
Jim Shaw <jimxshaw@gmail.com>
2015-06-16 20:39:39 -0500

    finished Pause, Stop and Play

--- audio-player/js/script.js
+++ audio-player/js/script.js
@@ -1,1 +1,43 @@
+var audio = new Audio('media/artist - firstSong.mp3');
+
+// Hide Pause button
+$('#pause').hide();
+
+// Play button hides and Pause button shows
+// while the song is playing
+$('#play').click(function() {
+  audio.play();
+  $('#play').hide();
+  $('#pause').show();
+});
+
+// Pause button hides and Play button shows
+// while the song is paused
+$('#pause').click(function() {
+  audio.pause();
+  $('#play').show();
+  $('#pause').hide();
+});
+
+// Stop functionality doesn't exist in jQuery
+// thus pause used in conjunction with setting
+// the song timer to zero will serve as "stop"
+$('#stop').click(function() {
+  audio.pause();
+  audio.currentTime = 0;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
