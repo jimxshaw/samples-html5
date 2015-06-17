4c3576f8ec6b2396bc8c1264a702b9e56883fdee
Jim Shaw <jimxshaw@gmail.com>
2015-06-16 21:09:52 -0500

    finished duration bar

--- audio-player/js/script.js
+++ audio-player/js/script.js
@@ -9,6 +9,7 @@ $('#play').click(function() {
   audio.play();
   $('#play').hide();
   $('#pause').show();
+  showDuration();
 });
 
 // Pause button hides and Play button shows
@@ -27,9 +28,31 @@ $('#stop').click(function() {
   audio.currentTime = 0;
 });
 
+// Volume control
+$('#volume').change(function() {
+  audio.volume = parseFloat(this.value / 10);
+});
 
+// Time/Duration
+function showDuration() {
+  $(audio).bind('timeupdate', function() {
+    // Get hours and minutes
+    var s = parseInt(audio.currentTime % 60);
+    var m = parseInt(audio.currentTime / 60) % 60;
 
+    if (s < 10) {
+      s = '0' + s;
+    }
+    $('#duration').html(m + ':' + s);
 
+    var value = 0;
+
+    if (audio.currentTime > 0) {
+      value = Math.floor((100) / audio.duration * audio.currentTime);
+    }
+    $('#progress').css('width', value + '%');
+  });
+}
 
 
 
