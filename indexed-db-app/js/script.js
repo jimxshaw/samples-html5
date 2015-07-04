374c9e082eae1011736ddc77abc9cfbd90be9707
Jim Shaw <jimxshaw@gmail.com>
2015-07-04 18:39:31 -0500

    created Object Store

--- indexed-db-app/js/script.js
+++ indexed-db-app/js/script.js
@@ -2,16 +2,29 @@ $(document).ready(function() {
   // Open database
   var request = indexedDB.open('customermanager', 1);
 
+  request.onupgradeneeded = function(e) {
+    var db = e.target.result;
+
+    if (!db.objectStoreNames.contains('customers')) {
+      var os = db.createObjectStore('customers', {keyPath: "id", autoIncrement: true});
+      // Create index for name
+      os.createIndex('name', 'name', {unique: false});
+    }
+  };
+
   // Success
   request.onsuccess = function(e) {
     console.log('Success: Open database...');
     db = e.target.result;
     // Show customers
-    showCustomers();
+    // showCustomers();
   };
 
   // Error
-  request.onerror = function() {
+  request.onerror = function(e) {
     console.log('Error: Could not open database...');
   };
+
+
+
 });
